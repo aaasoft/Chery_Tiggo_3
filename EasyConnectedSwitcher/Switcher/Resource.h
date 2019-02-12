8496701c70d136eead1c0cfdf65b66bf43d12568
张鹏 <scbeta@qq.com>
2019-02-12 12:58:32 +0800

    让播放的声音文件嵌入到程序资源中。

--- EasyConnectedSwitcher/Switcher/Resource.h
+++ EasyConnectedSwitcher/Switcher/Resource.h
@@ -3,6 +3,7 @@
 // Used by Switcher.rc
 //
 #define IDI_SWITCHER                    101
+#define IDR_WAVE_BTN                    132
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -10,8 +11,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
